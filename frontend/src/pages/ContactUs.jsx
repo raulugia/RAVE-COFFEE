5d5cedfb9af028f5f02a2bb0590369ea5e2668a6
Raul <raul.ugia@gmail.com>
2024-11-13 19:13:27 +0100

    ABoutUs page almost finished - todo:responsiveness

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import MainCard from '../components/MainCard'
 import telephone from "../assets/Telephone.png"
+import underline from "../assets/Underline.png"
 
 const ContactUs = () => {
   return (
@@ -8,6 +9,71 @@ const ContactUs = () => {
         <div className="flex flex-col md:flex-row mb-10 md:mb-20 w-full max-h-[450px]">
             <MainCard header="GET IN TOUCH..." text="We are available via email or telephone. Please see below for our opening hours."/>
             <img src={telephone} alt="" className='md:w-1/2 object-cover'/>
+        </div>
+        <div className='px-5 max-w-[1024px] mx-auto mb-10'>
+            <div>
+                <div className='mb-3'>
+                    <h1 className="text-4xl font-permanent-marker">CUSTOMER SERVICE</h1>
+                    <img src={underline} alt="line" />
+                </div>
+
+                <div className='flex gap-10'>
+                    <div className='flex flex-col gap-8'>
+                        <div>
+                            <p className='font-permanent-marker text-lg'>EMAIL</p>
+                            <p className='font-fira'>info@ravecoffee.co.uk</p>
+                        </div>
+                        <div>
+                            <p className='font-permanent-marker text-lg'>OPENING</p>
+                            <p className='font-fira'>Monday - Friday 9am - 5pm</p>
+                            <p className='font-fira'>phones and email</p>
+                            <p className='font-fira'>(For Cafe see below)</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-8'>
+                        <div>
+                            <p className='font-permanent-marker text-lg'>WHATSAPP</p>
+                            <p className='font-fira'>+44 7535 017079</p>
+                        </div>
+                        <div>
+                            <p className='font-permanent-marker text-lg'>RETURNS</p>
+                            <p className='font-fira'>Rave Coffee HQ,</p>
+                            <p className='font-fira'>Phoenix Way,</p>
+                            <p className='font-fira'>Cirencester,</p>
+                            <p className='font-fira'>GL7 1QG</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <p className='font-permanent-marker text-lg'>TELEPHONE</p>
+                            <p className='font-fira'>+44 1285 651884</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='w-full bg-gray-400 h-[1px] my-10'></div>
+
+            <div className='flex flex-col gap-8'>
+                <div>
+                    <p className='font-permanent-marker text-lg'>MARKETING / BRAND PARTNERSHIPS / PR</p>
+                    <p className='font-fira'>marketing@ravecoffee.co.uk</p>
+                </div>
+                <div>
+                    <p className='font-permanent-marker text-lg'>WHOLESALE</p>
+                    <p className='font-fira'>wholesale@ravecoffee.co.uk</p>
+                </div>
+            </div>
+
+            <div className='w-full bg-gray-400 h-[1px] my-10'></div>
+
+            <div>
+                <div>
+                    <p className='font-permanent-marker text-lg'>COMPANY DETAILS</p>
+                    <p className='font-fira'>Company Number: 07384599</p>
+                    <p className='font-fira'>VAT registered:  105657716</p>
+                </div>
+            </div>
         </div>
     </div>
   )
