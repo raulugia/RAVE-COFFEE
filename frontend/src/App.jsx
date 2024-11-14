0eb65a65e954b7692ad38a306d317fda4dd4c8fc
Raul U. M. <raul.ugia@hotmail.com>
2024-11-14 18:32:16 +0100

    models created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import About from './pages/About'
 import ContactUs from './pages/ContactUs'
 import Navbar from './components/Navbar'
 
-//comment
+//Xv2tEXMfxcHeDyui
 
 function App() {
   return (
