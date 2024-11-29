6b84a6973257ff1059efe0fa5a5d51c3c9934891
Raul U. M <raul.ugia@hotmail.com>
2024-11-29 13:33:23 +0100

    stripe installed in frontend - basic config

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -5,19 +5,28 @@ import { useAuth } from '@clerk/clerk-react'
 import axiosInstance from '../utils/axiosInstance'
 import Loading from '../components/Loading'
 import CheckoutDetailsCard from '../components/CheckoutDetailsCard'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+import {PaymentElement} from '@stripe/react-stripe-js';
+
+
+const STRIPE_PUBLISHABLE_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
+const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY)
 
 const Checkout = () => {
     const [loading, setLoading] = useState(false)
-    const { getToken} = useAuth()
+    const { getToken } = useAuth()
 
   return (
-    <div className='mt-10 flex justify-between min-h-[calc(100vh-140px)]'>
-        <CheckoutDetailsCard />
-        <CheckoutBasket />
-        {
+    <Elements stripe={stripePromise}>
+      <div className='mt-10 flex justify-between min-h-[calc(100vh-140px)]'>
+          <CheckoutDetailsCard />
+          <CheckoutBasket />
+          {
             loading && <Loading />
-        }
-    </div>
+          }
+      </div>
+    </Elements>
   )
 }
 
