adc562f8753a77e85d0ddd490b53fed489811bbc
Raul U. M <raul.ugia@hotmail.com>
2024-12-06 13:51:31 +0100

    Subscriptions responsiveness

--- frontend/src/pages/Subscriptions.jsx
+++ frontend/src/pages/Subscriptions.jsx
@@ -35,14 +35,14 @@ const Subscriptions = () => {
             </div>
         </div>
 
-        <div className='w-full h-[800px] relative'>
+        <div className='w-full h-[600px] md:h-[800px] relative'>
             <img src={giftPic} alt=""  className='h-full w-full object-cover object-center'/>
-            <div className='absolute top-0 px-10 w-[35%] h-full flex items-center'>
+            <div className='absolute top-0 px-10 md:w-[35%] max-w-[550px] h-full flex items-center'>
                 <div className='font-fira text-white bg-black py-12 relative'>
                     <div className='px-10 mt-5'>
                         <h3 className='font-permanent-marker text-4xl mb-10'>The gift of coffee...</h3>
                         <p>Why not give a budding bean buff a 3, 6 or 12 month gift coffee subscription? Our monthly deliveries will take them on a journey of discovery and teach them a thing or two along the way.</p>
-                        <div className='font-fira mt-8'>
+                        <div className='font-fira mt-8 hidden md:flex md:flex-col'>
                             {
                                 gift_text.map((text, index) => (
                                     <div className='flex gap-2 items-center'>
