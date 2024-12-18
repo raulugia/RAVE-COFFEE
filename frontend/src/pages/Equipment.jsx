367af84085c7a55a3e9d0e78a9915756800753fb
Raul U. M <raul.ugia@hotmail.com>
2024-12-18 12:55:40 +0100

    ItemCardSkeleton created and added to Equipment

--- frontend/src/pages/Equipment.jsx
+++ frontend/src/pages/Equipment.jsx
@@ -4,18 +4,20 @@ import EquipmentCard from '../components/EquipmentCard'
 import equipmentBanner from '../assets/Equipment-Banner.jpg'
 import axiosInstance from '../utils/axiosInstance'
 import { useBasket } from '../context/BasketContext'
+import ItemCardSkeleton from '../components/skeletons/ItemCardSkeleton'
 
 export const Equipment = () => {
     const [allEquipments, setAllEquipments] = useState([])
     const { setErrorData } = useBasket()
+    const [loading, setLoading] = useState(false);
     
-    //fetch all coffees and update state
     useEffect(() => {
         (
             async() => {
                 try{
                     setErrorData(null)
-                    
+                    setLoading(true)
+
                     let allEquipmentCategories = []
                     let arrangedData = []
                     const { data } = await axiosInstance.get('/equipment')
@@ -37,6 +39,8 @@ export const Equipment = () => {
                         header: "Error fetching data",
                         text: "There was an error getting the data. Please try again",
                     })
+                }finally{
+                    setLoading(false)
                 }
             }
         )()
@@ -47,25 +51,31 @@ export const Equipment = () => {
         <MainBanner cardHeader="COFFEE EQUIPMENT" cardText="Top-picks from our coffee gear range" imgSrc={equipmentBanner} imgAlt="equipment banner"/>
 
         <div className='px-5 mx-auto mb-10 max-w-[1550px]'>
-            <div className='flex flex-col items-center lg:items-stretch lg:flex-row flex-wrap md:gap-10'>
-                {
-                    allEquipments.map((equipment, index) => (
-                        <div key={index}>
-                            <div className='mb-5 md:mb-10'>
-                                <h3 className='font-permanent-marker text-3xl mb-2'>{equipment.type}</h3>
-                                <div className='w-full h-[4px] bg-mustard'></div>
-                            </div>
-                            <div className='flex flex-col items-center lg:items-stretch lg:flex-row flex-wrap md:gap-10'>
-                                {
-                                    equipment.items.map(item => (
-                                        <EquipmentCard key={index + item.name} {...item} />
-                                    ))
-                                }
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
+            {
+                loading ? (
+                    <ItemCardSkeleton type="equipment" />
+                ) : (
+                    <div className='flex flex-col items-center lg:items-stretch lg:flex-row flex-wrap md:gap-10'>
+                        {
+                            allEquipments.map((equipment, index) => (
+                                <div key={index}>
+                                    <div className='mb-5 md:mb-10'>
+                                        <h3 className='font-permanent-marker text-3xl mb-2'>{equipment.type}</h3>
+                                        <div className='w-full h-[4px] bg-mustard'></div>
+                                    </div>
+                                    <div className='flex flex-col items-center lg:items-stretch lg:flex-row flex-wrap md:gap-10'>
+                                        {
+                                            equipment.items.map(item => (
+                                                <EquipmentCard key={index + item.name} {...item} />
+                                            ))
+                                        }
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                )
+            }
         </div>
     </div>
   )
